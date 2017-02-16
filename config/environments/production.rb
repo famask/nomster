abc9e54094958db10dbef440becfeedcca069cef
Faisal <faisal.abdullah@gmail.com>
2017-02-16 12:49:02 +0000

    Made correction to config.action_mailer on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   }
 
 
-  #config.action_mailer.default_url_options = { host: 'nomster-famask.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'nomster-famask.herokuapp.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
